11f6ac7dad8d33ccf4ee3c7ded912854130631e0
Niv Ozer <niv.ozer@yahoo.com>
2024-03-03 22:23:36 +0200

    Success works but adding doesnt - a problem with Haim's tests

--- calorie-managment/public/idb.js
+++ calorie-managment/public/idb.js
@@ -1,26 +1,45 @@
+const openCalorisDB = (dbname, dbversion) => {
+  //Opening the data-base and if it doesnt exist - create it
+  const request = indexedDB.open(dbname, dbversion);
 
-  const openCalorisDB = (dbname, dbversion) => {
-    const request = indexedDB.open(dbname, dbversion);
-  
-    request.onerror = (event) => {
-      console.error("Why didn't you allow my web app to use IndexedDB?!");
-    };
-      request.onupgradeneeded = (event) => {
-        // Save the IDBDatabase interface
-        const db = event.target.result;
-        // Create an objectStore for this database
-        const calorieStore = db.createObjectStore('calories', { keyPath: 'id', autoIncrement: true });
-        calorieStore.createIndex('numofcaloriesIndex', 'numofcalories');
-        calorieStore.createIndex('categoryIndex', 'category');
-        calorieStore.createIndex('descriptionIndex', 'description');
-      };
-    
-
-      request.onsuccess = (event) => {
-        // Save the IDBDatabase interface
-        db = event.target.result;
-    };
-
-    return request;
-
+  request.onerror = (event) => {
+    console.error("Why didn't you allow my web app to use IndexedDB?!");
   };
+  request.onupgradeneeded = (event) => {
+    // Save the IDBDatabase interface
+    const db = event.target.result;
+    // Create an objectStore for this database
+    const calorieStore = db.createObjectStore("Items", {
+      keyPath: "id",
+      // autoIncrement: true,
+    });
+    // calorieStore.createIndex("numofcaloriesIndex", "numofcalories");
+    // calorieStore.createIndex("categoryIndex", "category");
+    // calorieStore.createIndex("descriptionIndex", "description");
+  };
+
+  request.onsuccess = (event) => {
+    // Save the IDBDatabase interface
+    db = event.target.result;
+  };
+
+  return request;
+};
+
+const addCalories = (item) => {
+  let tx = db.transaction("Items", "readwrite");
+  tx.oncomplete = (ev) => {
+    console.log(ev);
+  };
+  tx.onerror = (error) => {
+    console.warn(error);
+  };
+  let store = tx.objectStore("Items");
+  let request = store.add(item);
+  request.onsuccess = (ev) => {
+    console.log("Successfully Added an Object");
+  };
+  request.onerror = (err) => {
+    console.log("Error in request to add");
+  };
+};
