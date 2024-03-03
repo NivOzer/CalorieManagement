08d0522637794e87a8f2642afaa6e5f1df97f65d
Niv Ozer <niv.ozer@yahoo.com>
2024-03-03 22:40:32 +0200

    Fixed assigning buttons with use state

--- calorie-managment/src/App.js
+++ calorie-managment/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { React } from "react";
+import { React, useState } from "react";
 import { TextField } from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -11,24 +11,46 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import Button from "@mui/material/Button";
 
 function App() {
+  const [numOfCalories, setNumOfCalories] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handlenumOfCaloriesChange = (e) => {
+    setNumOfCalories(e.target.value);
+  };
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
   return (
     <div className="App">
       <IconButton aria-label="delete" size="large">
         <DeleteIcon fontSize="inherit" />
       </IconButton>
-      <TextField id="filled-basic" label="Item" variant="filled" />
       <TextField
         id="filled-basic"
         label="Number of Calories"
         variant="filled"
+        value={numOfCalories}
+        onChange={handlenumOfCaloriesChange}
       />
-      <TextField id="filled-basic" label="Description" variant="filled" />
+      <TextField
+        id="filled-basic"
+        label="Description"
+        variant="filled"
+        value={description}
+        onChange={handleDescriptionChange}
+      />
       <FormControl variant="filled" sx={{ m: 0, minWidth: 120 }}>
         <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
         <Select
           labelId="demo-simple-select-filled-label"
           id="demo-simple-select-filled"
-          // value={"Category"}
+          value={category}
+          onChange={handleCategoryChange}
         >
           <MenuItem value={10}>Breakfast</MenuItem>
           <MenuItem value={20}>Lunch</MenuItem>
@@ -39,6 +61,7 @@ function App() {
       <Button variant="contained" color="primary">
         Submit
       </Button>
+      <h1>{description}</h1>
     </div>
   );
 }
