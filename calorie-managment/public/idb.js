886ba4f93b66adb240fc5adfbf4f075f804c6677
Niv Ozer <niv.ozer@yahoo.com>
2024-03-04 14:31:15 +0200

    Working  Test

--- calorie-managment/public/idb.js
+++ calorie-managment/public/idb.js
@@ -1,51 +1,3 @@
-
- 
-// const idb = {
-//   openCalorisDB: (dbname, dbversion) => {
-//     return new Promise((resolve, reject) => {
-//       const request = indexedDB.open(dbname, dbversion);
-
-//       request.onerror = (event) => {
-//         reject("Failed to open database");
-//       };
-
-//       request.onupgradeneeded = (event) => {
-//         const db = event.target.result;
-//         const calorieStore = db.createObjectStore('calories', { keyPath: 'id', autoIncrement: true });
-//         calorieStore.createIndex('numofcaloriesIndex', 'numofcalories');
-//         calorieStore.createIndex('categoryIndex', 'category');
-//         calorieStore.createIndex('descriptionIndex', 'description');
-//       };
-
-//       request.onsuccess = (event) => {
-//         const db = event.target.result;
-//         resolve(db);
-//       };
-//     });
-//   },
-
-//   addCalories: (numOfCalories, category, description) => {
-//     return new Promise((resolve, reject) => {
-//       const transaction = db.transaction(['calories'], 'readwrite');
-//       const store = transaction.objectStore('calories');
-//       const addRequest = store.add({ numOfCalories, category, description });
-
-//       addRequest.onerror = (event) => {
-//         reject("Failed to add calories");
-//       };
-
-//       addRequest.onsuccess = (event) => {
-//         resolve("Calories added successfully");
-//       };
-//     });
-//   }
-
-
-// };
-
-
-
- 
 const idb = {
   openCalorisDB: (dbname, dbversion) => {
     return new Promise((resolve, reject) => {
@@ -57,10 +9,13 @@ const idb = {
 
       request.onupgradeneeded = (event) => {
         const db = event.target.result;
-        const calorieStore = db.createObjectStore('calories', { keyPath: 'id', autoIncrement: true });
-        calorieStore.createIndex('calorieIndex', 'calorie');
-        calorieStore.createIndex('categoryIndex', 'category');
-        calorieStore.createIndex('descriptionIndex', 'description');
+        const calorieStore = db.createObjectStore("calories", {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+        calorieStore.createIndex("calorieIndex", "calorie");
+        calorieStore.createIndex("categoryIndex", "category");
+        calorieStore.createIndex("descriptionIndex", "description");
       };
 
       request.onsuccess = (event) => {
@@ -68,8 +23,8 @@ const idb = {
         const dbObject = {
           addCalories: (calorieData) => {
             return new Promise((resolve, reject) => {
-              const transaction = db.transaction(['calories'], 'readwrite');
-              const store = transaction.objectStore('calories');
+              const transaction = db.transaction(["calories"], "readwrite");
+              const store = transaction.objectStore("calories");
               const addRequest = store.add(calorieData);
 
               addRequest.onerror = (event) => {
@@ -80,11 +35,10 @@ const idb = {
                 resolve("Calorie added successfully");
               };
             });
-          }
+          },
         };
         resolve(dbObject);
       };
     });
-  }
+  },
 };
-
